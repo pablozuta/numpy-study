0fd502eb283fc857931fed3204124726dd257338
pablozuta <pablo.zuta.h@gmail.com>
2023-05-30 18:34:58 -0400

    update

---
+++ generate_random_numbers.py
@@ -0,0 +1,6 @@
+import numpy as np 
+
+# generate an array of 5 random numbers between 0 and 1.
+random_numbers = np.random.rand(5)
+
+print(random_numbers)
---
+++ matrix_multiplication.py
@@ -0,0 +1,8 @@
+import numpy as np 
+
+matrix1 = np.array([[11, 14], [22, 32]])
+matrix2 = np.array([[33, 83], [46, 44]])
+
+result = np.dot(matrix1, matrix2)
+
+print(result)
