dc0b97199e23072d38f096442087c7d4aaf2f8f7
pablozuta <pablo.zuta.h@gmail.com>
2023-06-01 13:20:41 -0400

    se agrega 'logarithmic transformation'

---
+++ logarithmic_transformation.py
@@ -0,0 +1,10 @@
+import numpy as np 
+
+# create an array with positive values
+array = np.array([11, 44, 55, 22, 33])
+
+# perform element-wise logarithmic transformation
+log_array = np.log10(array)
+
+# mostrar resultados
+print(log_array)
